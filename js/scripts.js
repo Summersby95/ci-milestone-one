74f7022988c1bf459ffdadeaa76e8038a9c50649
Summersby95 <james.summersby@gmail.com>
2021-01-01 16:36:20 +0000

    feat: creat js/scripts.js file

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+
+
+  // var light_progress_bar = document.getElementById('#light-side-bar');
+  // var light_100_answer = $('#light_answer');
+  $('#light_answer').click(function() {
+    alert('Hello');
+  });
+  alert('Hello');
+
+  // light_100_answer.addEventListener('click', function() {
+  //   alert("Hello WOrld");
+  //   // light_progress_bar.width = '100%';
+  // });
